4d31afe03fe688cf4e2937ffa7906ff154929774
Stephen Liberty <stephen@liberty-irm.com>
2015-05-08 14:19:08 -0700

    Removed pin screen (which no longer exists), updated common login logic

--- common/workflow/login.js
+++ common/workflow/login.js
@@ -3,10 +3,11 @@ var config = require('config');
 module.exports = function (driver, username, password) {
     return driver
         .eval("window.location.hash='#/login'")
+        .waitForElementByCssSelector('#login-feature', 10000)
         .elementByCssSelector('#login-feature input[type=email]')
         .sendKeys(username)
         .elementByCssSelector('#login-feature input[type=password]')
         .sendKeys(password)
-        .elementByCssSelector('#login-feature button')
-        .click();
+        .elementByCssSelector('#login-feature form')
+        .submit();
 }
--- tests/features/login/index.js
+++ tests/features/login/index.js
@@ -17,18 +17,14 @@ describe('features.login', function () {
 
     //Here am getting Splash Screen Error 
     it('will log in with the correct username/password', function () {
-        return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(20000)
-        .eval("window.location.hash='#/splash'")
-        .waitForElementByCssSelector('#splash-feature', 10000).sleep(10000)
-        .eval("window.location.hash='#/dash'").sleep(10000);
-        
-        
+        return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password)
+            .waitForElementByCssSelector('#first-run-feature', 20000);
     });
 
     //Here test case is pass but am unable to see the Error message while runing the app
     it('will give an error if you try to login with the wrong username and password', function () {
         return loginWorkflow(driver, 'wrong@wrong.com', 'wrong')
-            .waitForElementByCssSelector('#login-feature .alert', 10000).sleep(10000);
+            .waitForElementByCssSelector('#login-feature .alert', 10000);
     });
 
 });
