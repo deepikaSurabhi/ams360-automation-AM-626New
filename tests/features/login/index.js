d9593f1da6d8ee67f7993360a87732db91be6898
Deepika <surabhde@vertafore.com>
2015-05-12 16:49:37 +0530

    AM435 - Login test cases for single and multi agencys

--- tests/features/login/index.js
+++ tests/features/login/index.js
@@ -14,19 +14,45 @@ describe('features.login', function () {
         return driver.eval("window.location.hash='#/login?reset'");
     });
 
-    //Here am getting Splash Screen Error 
-    it('will log in with the correct username/password', function () {
+    it('will log in with the correct username/password for single agency', function () {
         return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000);
     });
 
+    it('will log in with the correct username/password for multi agency', function () {
+        return loginWorkflow(driver, users.multiAgency.username, users.multiAgency.password).sleep(10000)
+        .waitForElementByCssSelector('#first-run-feature', 10000);
+       
+    });
+
     it('will give an error if you try to login with the wrong username and password', function () {
-        return loginWorkflow(driver, 'wrong@wrong.com', 'wrong');
-            //.waitForElementByCssSelector('#login-feature .alert', 10000).sleep(10000);
+        return loginWorkflow(driver, 'wrong@wrong.com', 'wrong')
+        .waitForElementByCssSelector('#login-feature .alert', 10000);
+    });
+
+    it('will give an error if you try to login without username and password', function () {
+        return loginWorkflow(driver, '', '').sleep(10000)
+        .waitForElementByCssSelector('#login-feature .alert', 10000);
+    });
+
+    it('will give an error if you try to login with username and without password for single agency', function () {
+        return loginWorkflow(driver, users.singleAgency.username, '').sleep(10000)
+        .waitForElementByCssSelector('#login-feature .alert', 10000);
+    });
+
+     it('will give an error if you try to login with username and without password for multi agency', function () {
+        return loginWorkflow(driver, users.multiAgency.username, '').sleep(10000)
+        .waitForElementByCssSelector('#login-feature .alert', 10000);
+    });
+
+    it('will give an error if you try to login without username and with password for single agency', function () {
+        return loginWorkflow(driver, '', users.singleAgency.password).sleep(10000)
+        .waitForElementByCssSelector('#login-feature .alert', 10000);
+    });
+
+    it('will give an error if you try to login without username and with password for multi agency', function () {
+        return loginWorkflow(driver, '', users.multiAgency.password).sleep(10000)
+        .waitForElementByCssSelector('#login-feature .alert', 10000);
     });
     
-    it('will give an error if you try to login with out the username and password', function () {
-        return loginWorkflow(driver, ' ', ' ');
-            //.waitForElementByCssSelector('#login-feature .alert', 10000).sleep(10000);
-    });
 
 });
