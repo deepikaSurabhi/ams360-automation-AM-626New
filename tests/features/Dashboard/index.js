0616ae5e78f17a57a0ffde54d77a7bbf90b43a94
Spandan <kashasp@vertafore.com>
2015-06-02 10:13:03 +0530

    Clicking on ToDo in Dashboard

---
+++ tests/features/Dashboard/index.js
@@ -0,0 +1,56 @@
+var loginWorkflow = require('common/workflow/login');
+
+var Q = require('q');
+var users = require('data/users');
+
+describe(
+		'features.login',
+		function() {
+			var driver;
+			before(function() {
+				return require('helpers/driver').init().then(function(d) {
+					driver = d;
+				})
+			});
+
+			beforeEach(function() {
+				return driver.eval("window.location.hash='#/login'").sleep(
+						20000);
+
+			});
+
+			it(
+					'login with single user and will click on the ToDo link on the dashboard',
+					function() {
+						return loginWorkflow(driver,
+								users.singleAgency.username,
+								users.singleAgency.password).sleep(20000)
+								.waitForElementByCssSelector('#splash-feature',20000).sleep(10000)
+								.eval("window.location.hash='#/dash'").sleep(10000)
+								.elementByXPath('//*[@id="dashboard"]/div[1]/div[1]/h5').click()
+								.sleep(10000);
+
+					});
+
+			it(
+					'login with multi agency user and will click on the ToDo link on the dashboard',
+					function() {
+						return loginWorkflow(driver,
+								users.multiAgency1.username,
+								users.multiAgency1.password)
+								.sleep(10000)
+								.elementByXPath('//*[@id="pick-agency-feature"]/div[1]/div/select/option[2]')
+								.click()
+								.sleep(5000)
+								.elementByCssSelector('#pick-agency-feature button')
+								.click()
+								.waitForElementByCssSelector('#splash-feature',20000)
+								.eval("window.location.hash='#/dash'")
+								.sleep(20000)
+								.elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[4]/button')
+								.click().sleep(10000).eval("window.location.hash='#/dash'").sleep(20000)
+								.elementByXPath('//*[@id="dashboard"]/div[1]/div[1]/h5').click()
+								.sleep(10000);
+					});
+
+		});
