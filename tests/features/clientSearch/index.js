75cc7fb04ed8f002f9e91c0cb4ecf5a7fb034581
Deepika <surabhde@vertafore.com>
2015-06-11 10:04:03 +0530

    Updated AM-441

--- tests/features/clientSearch/index.js
+++ tests/features/clientSearch/index.js
@@ -12,38 +12,30 @@ describe('features.clientSearch', function () {
     });
 
     beforeEach(function () {
-        return driver.eval("window.location.hash='#/login'").sleep(10000);
+        return driver.eval("window.location.hash='#/login'").sleep(10000)
+        .then(function () {
+       return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000)
+              .elementByXPath('//*[@id="eula-agreement-feature"]/footer/button[1]').click().click().sleep(5000)
+              .waitForElementByCssSelector('#splash-feature', 20000).sleep(20000)
+              .eval("window.location.hash='#/dash'").sleep(20000)
+              .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
+              .eval("window.location.hash='#/search'");
+              
+        });
     });
     it('will search for the client', function () {
-        return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000)
-        .waitForElementByCssSelector('#splash-feature', 20000).sleep(20000)
-        .eval("window.location.hash='#/dash'").sleep(20000)
-        .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
-        .eval("window.location.hash='#/search'")
-        .elementByCssSelector('#search input[placeholder="Search"]')
+        return elementByCssSelector('#search input[placeholder="Search"]').sleep(10000)
         .sendKeys("tim").click().sleep(5000)
         .elementByCssSelector('li:nth-child(3)').click().sleep(10000);
     });
+    
     it('will select the recent client', function () {
-        return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000)
-        .waitForElementByCssSelector('#splash-feature', 20000).sleep(20000)
-        .eval("window.location.hash='#/dash'").sleep(20000)
-        .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
-        .eval("window.location.hash='#/search'")
-        .elementByCssSelector('#search input[placeholder="Search"]').sleep(10000)
+        return elementByCssSelector('#search input[placeholder="Search"]').sleep(10000) 
         .elementByCssSelector('li:nth-child(3)').click().sleep(10000);
-        
-       
     });
     
     it('will disply the error message if we are searching for invalid user', function () {
-        
-        return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000)
-        .waitForElementByCssSelector('#splash-feature', 20000).sleep(20000)
-        .eval("window.location.hash='#/dash'").sleep(10000)
-        .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
-        .eval("window.location.hash='#/search'").sleep(2000)
-        .elementByCssSelector('#search input[placeholder="Search"]')
+        return elementByCssSelector('#search input[placeholder="Search"]').sleep(10000) 
         .sendKeys("aaaa").click().sleep(5000)
         //.elementByXPath('//*[@id="search"]/div[2]/div/div[2]/ul/li')
         .assert.isFulfilled('No clients found', 'No clients found', "done");
