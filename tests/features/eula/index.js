099a6c14b1e16920c1d774801048e2406ba21515
Deepika <surabhde@vertafore.com>
2015-06-15 10:49:30 +0530

    AM-638 User story

--- tests/features/eula/index.js
+++ tests/features/eula/index.js
@@ -14,13 +14,13 @@ describe('features.eula', function () {
         return driver.eval("window.location.hash='#/login'").sleep(10000);
     });
     
-    xit('Will redirected to login page if we disagree the EULA', function () {
+    it('Will redirected to login page if we disagree the EULA', function () {
         return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000)
         .elementByXPath('//*[@id="eula-agreement-feature"]/footer/button[2]').click().click().sleep(5000)
         .eval("window.location.hash='#/login'");
     });
     
-    xit('Will redirected to splash screen if we agree the EULA', function () {
+    it('Will redirected to splash screen if we agree the EULA', function () {
         return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(10000)
               .elementByXPath('//*[@id="eula-agreement-feature"]/footer/button[1]').click().click().sleep(5000)
               .waitForElementByCssSelector('#splash-feature', 20000).sleep(20000)
