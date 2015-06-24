69d047d4c74e6fc13ce82c36ace5012729db6892
Deepika <surabhde@vertafore.com>
2015-06-24 14:51:07 +0530

    Updated

--- tests/features/ClientRightPane/index.js
+++ tests/features/ClientRightPane/index.js
@@ -1,6 +1,7 @@
 var loginWorkflow = require('common/workflow/login');
 var Q = require('q');
 var users = require('data/users');
+var constants = require('data/constants');
 var assert = require('assert');
 
 describe('features.ClientRightPane', function () {
@@ -24,7 +25,7 @@ describe('features.ClientRightPane', function () {
               .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
               .eval("window.location.hash='#/search'")
               .elementByCssSelector('#search input[placeholder="Search"]').sleep(5000)
-              .sendKeys(users.clientSearch.name).click().sleep(5000)
+              .sendKeys(constants.clientSearch.name).click().sleep(5000)
               .elementByCssSelector('li:nth-child(1)').click().sleep(10000)
               .elementByXPath('//*[@id="master-layout"]/div[2]/md-sidenav/md-content/div/div[1]/button').click().click().sleep(20000);
     });
@@ -36,32 +37,31 @@ describe('features.ClientRightPane', function () {
               .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
               .eval("window.location.hash='#/search'")
               .elementByCssSelector('#search input[placeholder="Search"]').sleep(5000)
-              .sendKeys(users.clientSearch.name).click().sleep(5000)
+              .sendKeys(constants.clientSearch.name).click().sleep(5000)
               .elementByCssSelector('li:nth-child(2)').click().sleep(10000)
-              .elementByXPath('//*[@id="master-layout"]/div[2]/md-sidenav/md-content/div/div[2]/button').click().click().sleep(10000)
-              .expect($('#title')).to.have.css('#client-summary h1');
+              .elementByXPath('//*[@id="master-layout"]/div[2]/md-sidenav/md-content/div/div[2]/button').click().click().sleep(10000);
     });
     
-    xit('will select the client Claims', function () {
+    it('will select the client Claims', function () {
         return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(5000)
               .waitForElementByCssSelector('#splash-feature', 20000).sleep(10000)
               .eval("window.location.hash='#/dash'").sleep(10000)
               .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
               .eval("window.location.hash='#/search'")
               .elementByCssSelector('#search input[placeholder="Search"]').sleep(5000)
-              .sendKeys(users.clientSearch.name).click().sleep(5000)
+              .sendKeys(constants.clientSearch.name).click().sleep(5000)
               .elementByCssSelector('li:nth-child(1)').click().sleep(10000)
               .elementByXPath('//*[@id="master-layout"]/div[2]/md-sidenav/md-content/div/div[3]/button').click().click().sleep(10000);
     });
     
-    xit('will select the client Documents', function () {
+    it('will select the client Documents', function () {
         return loginWorkflow(driver, users.singleAgency.username, users.singleAgency.password).sleep(5000)
               .waitForElementByCssSelector('#splash-feature', 20000).sleep(10000)
               .eval("window.location.hash='#/dash'").sleep(10000)
               .elementByXPath('//*[@id="master-layout"]/div[1]/md-sidenav/md-content/div[2]/button').click().sleep(10000)
               .eval("window.location.hash='#/search'")
               .elementByCssSelector('#search input[placeholder="Search"]').sleep(5000)
-              .sendKeys(users.clientSearch.name).click().sleep(5000)
+              .sendKeys(constants.clientSearch.name).click().sleep(5000)
               .elementByCssSelector('li:nth-child(1)').click().sleep(10000)
               .elementByXPath('//*[@id="master-layout"]/div[2]/md-sidenav/md-content/div/div[4]/button').click().click().sleep(10000);
         
